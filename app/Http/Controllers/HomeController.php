507d875b7d742947736d5a5f4b38e47336318367
preethal <preethaljayadevan@gmail.com>
2021-10-19 19:02:31 +0530

    changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,32 +28,32 @@ class HomeController extends Controller
     
 
     public function store()
-    {
+        {
        // Api uthentication with username and password
-    $response = Http::withBasicAuth('SPSAPI', 'sps2021?')->get( 'https://rsrge1testapi.snapfulfil.net/api/shipments/');
+        $response = Http::withBasicAuth('SPSAPI', 'sps2021?')->get( 'https://rsrge1testapi.snapfulfil.net/api/shipments/');
 
         if ($response->successful())
          {
 
         $shipments = json_decode($response->getBody()->getContents(),  true);
-         foreach($shipments as $shipment){
-                // Update the shipment if exist or create new shipment
-                $saveShipment = new Shippment;
-                        $saveShipment->where('ShipmentId', $shipment['ShipmentId'])->first();
-                        $saveShipment->BizId = $shipment['BizId'];
-                        $saveShipment->BizSalesOrder = $shipment['BizSalesOrder'];
-                        $saveShipment->Status = $shipment['Status'];
-                        $saveShipment->ShipmentId = $shipment['ShipmentId'];
-                        $saveShipment->save();
+        foreach($shipments as $shipment){
+        // Update the shipment if exist or create new shipment
+        $saveShipment = new Shippment;
+        $saveShipment->where('ShipmentId', $shipment['ShipmentId'])->first();
+        $saveShipment->BizId = $shipment['BizId'];
+        $saveShipment->BizSalesOrder = $shipment['BizSalesOrder'];
+        $saveShipment->Status = $shipment['Status'];
+        $saveShipment->ShipmentId = $shipment['ShipmentId'];
+        $saveShipment->save();
         }
         }
-     else {
+        else {
 
         // Return Error message
         }
         return redirect('home');
 
-     } 
+        } 
 
 
     public function index()
